d0558d21828f1c6e6863e54dd8cabf936f163191
Jonathan Picques <jonathan.picques@gmail.com>
2016-11-02 06:38:02 +0100

    Fixed dlopen on linux

--- engine/src/core/library.cpp
+++ engine/src/core/library.cpp
@@ -19,7 +19,7 @@ namespace dude {
         #ifdef DUDE_PLATFORM_WINDOWS
             _library = LoadLibrary(library_path.c_str());
         #else
-            _library = dlopen(library_path.c_str(), RTLD_LOCAL | RTLD_LAZY);
+            _library = dlopen((library_path + ".so").c_str(), RTLD_LOCAL | RTLD_LAZY);
         #endif
         assert(_library != nullptr);
     }
@@ -37,7 +37,7 @@ namespace dude {
         #ifdef DUDE_PLATFORM_WINDOWS
             void_symbol = reinterpret_cast<void *>(GetProcAddress(reinterpret_cast<HMODULE>(_library), symbol.c_str()));
         #else
-            void_symbol = reinterpret_cast<void *>(dlsym(library, symbol.c_str()));
+            void_symbol = reinterpret_cast<void *>(dlsym(_library, symbol.c_str()));
         #endif
         assert(void_symbol != nullptr);
         return void_symbol;
