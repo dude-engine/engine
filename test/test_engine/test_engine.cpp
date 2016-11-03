8349eb4a90cbe64903e98126e8ef8e48edb7e1d8
Jonathan Picques <jonathan.picques@gmail.com>
2016-11-03 11:15:33 +0100

    Handle emscripten filesystem on build

--- test/test_engine/test_engine.cpp
+++ test/test_engine/test_engine.cpp
@@ -10,10 +10,9 @@
 #include <dude/behavior/behavior.hpp>
 
 TEST(Engine, Library) {
-    auto library = dude::library("../../plugins/input/libplugin_input");
+    auto library = dude::library("plugins/input/libplugin_input");
     auto behavior_creator = library.symbol<dude::behavior *(*)()>("create");
     auto behavior = std::unique_ptr<dude::behavior>(behavior_creator());
-    behavior->name("test worked");
-    EXPECT_EQ(behavior->name(), "test worked");
+    EXPECT_TRUE(behavior->name().empty());
     EXPECT_DEATH_IF_SUPPORTED(library.symbol<int (*)()>("undefined_symbol"), "");
 }
