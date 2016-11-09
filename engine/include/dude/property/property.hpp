07d58f07f646c5f5e7d88709b05b331de57c90bf
JonathanPicques <jonathan.picques@gmail.com>
2016-11-09 20:09:50 +0100

    Moved property non template implementations in the cpp file

--- engine/include/dude/property/property.hpp
+++ engine/include/dude/property/property.hpp
@@ -9,31 +9,35 @@
 #include <cstddef>
 #include <tuple>
 
-namespace dude_impl {
+namespace dude {
 
-    class property_base {
-    public:
-        property_base() {};
-        virtual ~property_base() {};
+    namespace impl {
 
-    public:
-        virtual auto is(std::type_info const &type_info) const -> bool = 0;
-        virtual auto copy() const -> property_base * = 0;
-    };
+        class property_base {
+        public:
+            property_base();
+            virtual ~property_base();
 
-    template<typename T>
-    class property_data : public property_base, public std::tuple<T> {
-    public:
-        using std::tuple<T>::tuple;
+        public:
+            virtual auto is(std::type_info const &type_info) const -> bool = 0;
+            virtual auto copy() const -> property_base * = 0;
+        };
 
-    public:
-        auto get() -> T & { return std::get<0>(*this); }
-        auto get() const -> T const & { return std::get<0>(*this); }
+        template<typename T>
+        class property_data : public property_base, public std::tuple<T> {
+        public:
+            using std::tuple<T>::tuple;
 
-    public:
-        auto is(std::type_info const &type_info) const -> bool override final { return type_info.name() == typeid(T).name(); }
-        auto copy() const -> property_base * override final { return new property_data{get()}; }
-    };
+        public:
+            auto get() -> T & { return std::get<0>(*this); }
+            auto get() const -> T const & { return std::get<0>(*this); }
+
+        public:
+            auto is(std::type_info const &type_info) const -> bool override final { return type_info.name() == typeid(T).name(); }
+            auto copy() const -> property_base * override final { return new property_data{get()}; }
+        };
+
+    }
 
 }
 
@@ -43,20 +47,19 @@ namespace dude {
     public:
         template<typename T> using none_t = typename std::enable_if<!std::is_same<property, T>::value>::type;
         template<typename T> using decay_t = typename std::decay<T>::type;
-        using base_t = dude_impl::property_base *;
+        using base_t = dude::impl::property_base *;
 
     public:
-        template<typename T, typename U = decay_t<T>, typename = none_t<U>> property(T &&t) : _base{new dude_impl::property_data<U>{std::forward<T>(t)}} {}
+        template<typename T, typename U = decay_t<T>, typename = none_t<U>> property(T &&t) : _base{new dude::impl::property_data<U>{std::forward<T>(t)}} {}
 
     public:
-        property() {};
-        property(property const &p) : _base(p._base->copy()) {}
-        property(property &&p) : _base{p._base} { p._base = nullptr; }
-        ~property() { delete _base; }
+        property();
+        property(property const &p);
+        property(property &&p);
+        ~property();
 
     public:
-		property &operator=(property &p) { std::swap(_base, p._base); return *this; }
-        friend auto swap(property &a, property &b) -> void { std::swap(a._base, b._base); }
+		property &operator=(property &p);
 
     public:
         template<typename T> auto is() const -> bool { return _base != nullptr && _base->is(typeid(T)); }
@@ -65,23 +68,21 @@ namespace dude {
         template<typename T> auto get() & -> T & { return safe_get<T>(); };
         template<typename T> auto get() && -> T && { return std::move(safe_get<T>()); };
         template<typename T> auto get() const & -> T const & { return safe_get<T>(); };
-        // template<typename T> operator T() { return safe_get<T>(); };
 
     public:
-        template<typename T, typename U = decay_t<T>> auto set(T &&t) -> void { clear(); _base = new dude_impl::property_data<U>{std::forward<T>(t)}; }
+        template<typename T, typename U = decay_t<T>> auto set(T &&t) -> void { clear(); _base = new dude::impl::property_data<U>{std::forward<T>(t)}; }
         template<typename T> auto operator=(T &&t) -> void { set(std::forward<T>(t)); };
 
     public:
-        auto clear() -> void { delete _base; _base = nullptr; }
-        auto empty() const -> bool { return _base == nullptr; }
+        auto clear() -> void;
+        auto empty() const -> bool;
+
+    public:
+        friend auto swap(property &a, property &b) -> void;
 
     private:
-        template<typename T> auto safe_get() -> T & { return dynamic_cast<dude_impl::property_data<T> &>(*_base).get(); }
-        template<typename T> auto safe_get() const  -> T const & { return dynamic_cast<dude_impl::property_data<T> const &>(*_base).get(); }
-        /*
-        template<typename T> auto unsafe_get() -> T & { return static_cast<dude_impl::property_data<T> &>(*_base).get(); }
-        template<typename T> auto unsafe_get() const  -> T const & { return static_cast<dude_impl::property_data<T> const &>(*_base).get(); }
-         */
+        template<typename T> auto safe_get() -> T & { return dynamic_cast<dude::impl::property_data<T> &>(*_base).get(); }
+        template<typename T> auto safe_get() const  -> T const & { return dynamic_cast<dude::impl::property_data<T> const &>(*_base).get(); }
 
     private:
         base_t _base;
--- engine/src/property/property.cpp
+++ engine/src/property/property.cpp
@@ -3,3 +3,57 @@
 //
 
 #include <dude/property/property.hpp>
+
+namespace dude {
+
+    namespace impl {
+
+        property_base::property_base() {
+
+        }
+
+        property_base::~property_base() {
+
+        }
+
+    }
+
+}
+
+namespace dude {
+
+    property::property() {
+
+    }
+
+    property::property(property const &p) : _base(p._base->copy()) {
+
+    }
+
+    property::property(property &&p) : _base{p._base} {
+        p._base = nullptr;
+    }
+
+    property::~property() {
+        delete _base;
+    }
+
+    auto property::operator=(property &p) -> property & {
+        std::swap(_base, p._base);
+        return *this;
+    }
+
+    auto property::clear() -> void {
+        delete _base;
+        _base = nullptr;
+    }
+
+    auto property::empty() const -> bool {
+        return _base == nullptr;
+    }
+
+    auto swap(property &a, property &b) -> void {
+        std::swap(a._base, b._base);
+    }
+
+}
