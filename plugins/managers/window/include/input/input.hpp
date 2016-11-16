298157d7e4f1609f61a92ee46fd42933eded419e
Jonathan Picques <jonathan.picques@gmail.com>
2016-11-16 01:20:53 +0100

    added input manager in demo

---
+++ plugins/managers/window/include/input/input.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Jonathan Picques on 01/11/2016.
+//
+
+#pragma once
+
+#include <dude/plugin/manager.hpp>
+
+namespace dude {
+
+    class input final : public manager {
+    public:
+        input();
+        virtual ~input();
+    };
+
+}
