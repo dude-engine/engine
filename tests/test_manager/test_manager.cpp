8a17444b5005efca45bd14b8c64c92bb3f8ac48a
JonathanPicques <jonathan.picques@gmail.com>
2017-12-15 11:13:47 +0100

    Added manager test suite

---
+++ tests/test_manager/test_manager.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Jonathan Picques on 15-Dec-17.
+// Copyright (c) 2017 Jonathan Picques. All rights reserved.
+//
+
+#include <gtest/gtest.h>
+
+#include <dude/plugin/manager.hpp>
+
+namespace dude {
+
+    TEST(Manager, Basic) {
+
+    }
+
+}
